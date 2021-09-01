339c523fc055d420e3c82b682f900a4b61f7345f
jihyun <kjwise1128@gmail.com>
2021-09-02 01:03:01 +0900

    0901

--- src/block.js
+++ src/block.js
@@ -1,16 +1,90 @@
-const block = {
-    MagicNumber: "0xD9B4BEF9",
-    BlockSize: "4mb",
-    header: {
-        version:"1.0.0",
-        HashPreBlock:0x000000000, //제네시스블럭은 하드코딩으로 증명함
-        HashMerkleRoot:`SHA256`,
-        timestamp:`시간`,
-        bits:`작업증명 난이도를 정하는 `,
-        Nonce: `난수` //4byte 양수
-    },
-    body: ["hello world!"] //객체가 될수도, 배열이 될수도
-}
-//block의 형태는 고정된 형식이 있는게 아니라 우리가 마음대로 생성할 수 있음
+const fs = require('fs')
+const merkle = require('merkle')
+const CryptoJs = require('crypto-js')
 
-/* http request message */
+/* 사용법 */
+//const tree = merkle("sha256").sync([]) //배열을 넣어야함, 배열 안에 객체 넣는 것 가능, tree구조로 만들어줌
+//tree.root()
+
+class BlockHeader {
+    constructor( version, index, previousHash, time, merkleRoot ){
+        this.version = version // 1 {version:1}
+        this.index = index // 2 {version:1, index:2} // 포인트!
+        this.previousHash = previousHash // 마지막 블록을 가져올 수 있어야함 -> 그 블록에서 header의 정보만 들고 올줄 알아야함 -> 전부 다 string으로 연결할 줄 알아야함 -> 그것을 SHA256으로 변환할줄 알아야함
+        this.time = time
+        this.merkleRoot = merkleRoot
+    }
+}
+
+//const header = new BlockHeader(1,2,3,4,5)
+//console.log(header)
+
+// {
+//     version:1,
+//     index:2,
+//     previousHash:3,
+//     time:4,
+//     merkleRoot:5
+// }
+
+class Block {
+    constructor(header,body){
+        this.header = header;
+        this.body = body
+    }
+}
+
+// const blockchain = new Block(new BlockHeader(1,2,3,4,5),['hello'])
+// console.log(blockchain)
+
+let Blocks = [createGenesisBlock()]
+
+function getBlocks(){
+    return Blocks
+}
+
+function getLastBlock() {
+    return Blocks[Blocks.length -1]
+}
+
+function createGenesisBlock(){
+    // 1. header만들기
+    // 5개의 인자값을 만들어야됨
+    const version = getVersion() //1.0.0
+    const index = 0
+    const time = getCurrentTime()
+    const previousHash = '0'.repeat(64)
+    const body = ['hello block']
+
+    const tree = merkle('sha256').sync(body)
+    const root = tree.root() || '0'.repeat(64)
+
+    const header = new BlockHeader(version,index,previousHash,time,root)
+    return new Block(header,body)
+}
+
+// const block = [createGenesisBlock()]
+// console.log(block)
+
+function addBlock(){
+    //new header -> new block (header, body)
+}
+
+function getVersion(){
+    const {version} = JSON.parse(fs.readFileSync("../package.json"))
+    console.log(version)
+    return version
+
+    //const package = fs.readFileSync("../package.json")
+    //console.log(package.toString("utf8"))
+    //console.log(JSON.parse(package).version)
+    //return JSON.parse(package).version
+}
+
+function getCurrentTime(){
+    console.log( Math.ceil(new Date().getTime()/1000) )
+    return Math.ceil(new Date().getTime()/1000)
+}
+
+
+console.log(Blocks)
